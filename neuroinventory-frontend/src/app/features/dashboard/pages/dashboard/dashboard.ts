c53b42ed0cc0fdb12b2c108079415c2742a9e1ac
Ivan Lizzt <alejandrolistc@hotmail.com>
2026-09-24 17:04:16 -0500

    chore: Retira ficheros testapi

--- neuroinventory-frontend/src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ neuroinventory-frontend/src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,16 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { HttpTestService } from '../../../../core/http/http-test.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {
-  private readonly httpTestService = inject(HttpTestService);
-
-  protected testHttp(): void {
-    this.httpTestService.test();
-  }
-}
+export class Dashboard {}
