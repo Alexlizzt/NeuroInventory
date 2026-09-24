273cd8dce55a4ccd14e06d4bc92121ee957356e9
Ivan Lizzt <alejandrolistc@hotmail.com>
2026-09-24 16:45:32 -0500

    chore: Autorizando frontend para acceder a backend

--- inventory-service/src/main/java/com/alexlizzt/inventory_service/infraestructure/security/SecurityConfig.java
+++ inventory-service/src/main/java/com/alexlizzt/inventory_service/infraestructure/security/SecurityConfig.java
@@ -84,7 +84,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-            "http://localhost:5173",
+            "http://localhost:4200",
             "http://localhost:3000"
         ));
 
